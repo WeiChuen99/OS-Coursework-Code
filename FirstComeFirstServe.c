f4335f37f2d5fbf7de510d0f9b331882e1dee764
rz <lee.rz99@gmail.com>
2019-12-11 13:21:41 +0800

    Remove unwanted codes for fcfs

--- FirstComeFirstServe.c
+++ FirstComeFirstServe.c
@@ -1,7 +1,6 @@
 // C program for implementation of FCFS 
 // scheduling 
 #include<stdio.h>
-#include <ctype.h> 
 // Function to find the waiting time for all 
 // processes = array contain process id
 // n = 	number of element in the array
@@ -65,8 +64,6 @@ void findAverageTime( int processes[], int n, int burstTime[])
 int main() 
 { 
 	int numberOfProcesses,i,varForWhileLoop;
-
-	input:
 	printf("Please enter number of processes:" );
 	scanf("%d",&numberOfProcesses);
 	printf("%d\n",numberOfProcesses );
